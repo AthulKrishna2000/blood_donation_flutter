af925bd2dd566fa2b91886b1781221840ebb3228
athulbytes <athulbytes1@gmail.com>
2026-03-17 00:51:25 +0530

    Initial Flutter project setup

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: Color(0xFFF22255),
+    scaffoldBackgroundColor: Colors.white,
+  );
+}
