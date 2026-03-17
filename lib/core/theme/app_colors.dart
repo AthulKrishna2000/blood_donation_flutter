4173fe94afd48a80bf7f5273eb5b29d396e53b55
athulbytes <athulbytes1@gmail.com>
2026-03-18 04:02:04 +0530

    Day 1: Project structure and theme setup

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFF22255); // blood red
+  static const Color secondary = Color(0xFF2D2D2D);
+  static const Color background = Colors.white;
+  static const Color textDark = Colors.black;
+  static const Color textLight = Colors.white;
+}
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,8 +1,30 @@
+import 'package:blood_donation_app/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
+
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
-    primaryColor: Color(0xFFF22255),
-    scaffoldBackgroundColor: Colors.white,
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.background,
+    
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.primary,
+      elevation: 0,
+    ),
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+      ),
+    ),
+
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+    ),
   );
 }
